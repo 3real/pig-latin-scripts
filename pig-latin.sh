62b253ef4d25c5ab81e93758d5e8ddadf26ec912
Michael Everett <michaeleverett@Michaels-MBP.home>
2017-04-18 18:40:39 +0100

    pre-array script

--- pig-latin.sh
+++ pig-latin.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+#Use set -x to run line by line for debugging
 set -x
 
 #prompt for user input and line to record user input
@@ -17,7 +18,15 @@ echo $firstLetter
 serInput="${userInput:1}"
 echo $serInput
 
+# This code block uses grep to find vowels and uses the boolean result of grep to apply logic
+#echo $firstLetter | grep "^[aeiou]"
+#if [ $? = 0 ];then
+#	echo vowel
+#else
+#	echo constanant
+#fi
 
+# This code block copares firstLetter to a glob and then applies logic
 if [[ "$firstLetter" != *[aeiou]* ]];then
 	latin=$serInput$firstLetter"ay"
 	echo $latin
@@ -25,3 +34,9 @@ else
 	latin=$userInput"way"
 	echo $latin
 fi
+
+userArray=${userInput// /$'\n'}
+for word in $userArray
+do
+	echo "$word"
+done
